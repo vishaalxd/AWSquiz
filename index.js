77a4796112b91512d2d1aa2dbc8fe0f31f0173f7
Vishal Philip <vishaalxd@gmail.com>
2018-09-24 08:25:30 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Routes from './router';
+import registerServiceWorker from './registerServiceWorker';
+
+
+
+
+
+ReactDOM.render( < Routes / > , document.getElementById('root')); {
+    registerServiceWorker();
+}
